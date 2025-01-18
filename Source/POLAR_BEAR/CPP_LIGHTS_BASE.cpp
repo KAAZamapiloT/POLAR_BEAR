cc04cf6633a99683d8084e371fdec955a99bfe5e
Kazamapilot <202351150@iiitvadodara.ac.in>
2025-01-18 19:01:11 +0530

    enemy and its few functionality

---
+++ Source/POLAR_BEAR/CPP_LIGHTS_BASE.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_LIGHTS_BASE.h"
+
+
+// Sets default values
+ACPP_LIGHTS_BASE::ACPP_LIGHTS_BASE()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACPP_LIGHTS_BASE::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_LIGHTS_BASE::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
