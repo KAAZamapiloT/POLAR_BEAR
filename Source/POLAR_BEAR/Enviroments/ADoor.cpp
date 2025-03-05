ca1ffd289a36e6f1db4ae05ff9bc964bc005c51d
Kazamapilot <kazamapilot@gmail.com>
2025-03-06 00:07:17 +0530

    anim

--- Source/POLAR_BEAR/Enviroments/ADoor.cpp
+++ Source/POLAR_BEAR/Enviroments/ADoor.cpp
@@ -2,13 +2,18 @@
 
 
 #include "ADoor.h"
-
-
+#include"Components/BoxComponent.h"
+#include"Components/StaticMeshComponent.h"
+#include"Sound/SoundCue.h"
+#include"Components/AudioComponent.h"
 // Sets default values
 AADoor::AADoor()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	CollisionComponent=CreateDefaultSubobject<UBoxComponent>(TEXT("BOX COLLIDER"));
+	DoorMesh=CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DOOR MESH"));
+	AudioComponent=CreateDefaultSubobject<UAudioComponent>(TEXT("AUDIO"));
 }
 
 // Called when the game starts or when spawned
@@ -22,5 +27,25 @@ void AADoor::BeginPlay()
 void AADoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+bool AADoor::Open()
+{
+	return true;
+}
+
+bool AADoor::Close()
+{
+	return false;
+}
+
+bool AADoor::Unlock()
+{
+	return true;
+}
+
+bool AADoor::IsLocked()
+{
+	return bIsLocked;
 }
 
--- Source/POLAR_BEAR/Enviroments/ADoor.h
+++ Source/POLAR_BEAR/Enviroments/ADoor.h
@@ -5,21 +5,55 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "ADoor.generated.h"
-
+class UStaticMeshComponent;
+class UBoxComponent;
+class USoundCue;
+class UAudioComponent;
 UCLASS()
 class POLAR_BEAR_API AADoor : public AActor
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this actor's properties
-	AADoor();
+	//UPROPERTY(BlueprintReadWrite)
+	// UItem RequiredKey;
+	UPROPERTY(BlueprintReadWrite)
+	UBoxComponent*CollisionComponent;
+	
+	UPROPERTY(BlueprintReadWrite)
+	UStaticMeshComponent*DoorMesh;
+	
+	
+// sound for doors
+	UPROPERTY(BlueprintReadWrite,Category="Sounds")
+	UAudioComponent*AudioComponent;
+	UPROPERTY(BlueprintReadWrite,Category="Sounds")
+	USoundCue*OpenSound=nullptr;
+	UPROPERTY(BlueprintReadWrite,Category="Sounds")
+	USoundCue*CloseSound=nullptr;
+	UPROPERTY(BlueprintReadWrite,Category="Sounds")
+	USoundCue*LockSound=nullptr;
 
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsOpen=true;
+public:
+	// Sets default values for this actor's properties 
+	AADoor();
+     
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+    
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable,Blueprintable)
+	bool Open();
+	UFUNCTION(BlueprintCallable,Blueprintable)
+	bool Close();
+	UFUNCTION(BlueprintCallable,Blueprintable)
+	bool Unlock();
+	UFUNCTION(BlueprintCallable,Blueprintable)
+	bool IsLocked();
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsLocked=false;
 };
