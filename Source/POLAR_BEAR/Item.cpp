b68633337cb8e748e08e5fbb838a3b443a12471d
Kazamapilot <kazamapilot@gmail.com>
2025-03-09 14:55:33 +0530

    wrk

--- Source/POLAR_BEAR/Item.cpp
+++ Source/POLAR_BEAR/Item.cpp
@@ -2,13 +2,18 @@
 
 
 #include "Item.h"
-
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Engine/Texture.h"
 
 // Sets default values
 AItem::AItem()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	BoxCollision=CreateDefaultSubobject<UBoxComponent>(TEXT("BOX Collision"));
+	ItemMesh = CreateDefaultSubobject<UStaticMesh>(TEXT("Item Mesh"));
+	
 }
 
 // Called when the game starts or when spawned
--- Source/POLAR_BEAR/Item.h
+++ Source/POLAR_BEAR/Item.h
@@ -6,6 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+
+class UTexture;
+class UStaticMesh;
+class UBoxComponent;
+UENUM()
+enum ItemType
+{
+	Key,
+	Document,
+	Consumable
+};
 UCLASS()
 class POLAR_BEAR_API AItem : public AActor
 {
@@ -14,7 +25,13 @@ class POLAR_BEAR_API AItem : public AActor
 public:
 	// Sets default values for this actor's properties
 	AItem();
-
+protected:
+	UPROPERTY(BlueprintReadWrite)
+	FName ItemName;
+	UPROPERTY(BlueprintReadWrite)
+	ItemType Item;
+	UPROPERTY(BlueprintReadWrite)
+	FText ItemDescription;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,4 +39,13 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+UPROPERTY(BlueprintReadWrite)
+	bool bIsUsable;
+	//TO DO : FINDING APPROPRIATE TEXXTURES UPROPERTY(BlueprintReadWrite)
+	//UTexture*ItemIcon;
+	UPROPERTY(BlueprintReadWrite)
+    UStaticMesh*ItemMesh;
+	UPROPERTY(BlueprintReadWrite)
+	UBoxComponent*BoxCollision;
+	
 };
