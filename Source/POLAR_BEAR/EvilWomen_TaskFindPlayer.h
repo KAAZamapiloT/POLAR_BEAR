5b08e28aef85061635a18b9e60c3654466577922
Kazamapilot <kazamapilot@gmail.com>
2025-03-10 18:16:25 +0530

    EVIL WOMEN IS MOVING

---
+++ Source/POLAR_BEAR/EvilWomen_TaskFindPlayer.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "EvilWomen_TaskFindPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class POLAR_BEAR_API UEvilWomen_TaskFindPlayer : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+};
