7d919996775682bfdde03d4a50c695ae31062133
Kazamapilot <kazamapilot@gmail.com>
2025-03-16 18:33:36 +0530

    NEW DEVIL - INVENTORY COMPONENT

---
+++ Source/POLAR_BEAR/AInventoryComponent.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AInventoryComponent.h"
+
+
+// Sets default values for this component's properties
+UAInventoryComponent::UAInventoryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UAInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+void UAInventoryComponent::SetInventory(TArray<AActor*> NewInventory)
+{
+	Inventory = NewInventory;
+}
+
+bool UAInventoryComponent::bContains(AItemsBase* Item)
+{
+	return Inventory.Contains(Item);
+}
+
+void UAInventoryComponent::RemoveItem(AItemsBase* Item)
+{
+	Inventory.Remove(Item);
+}
+
+void UAInventoryComponent::AddItem(AItemsBase* item)
+{
+	Inventory.Add(item);
+}
+
+// Called every frame
+void UAInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                         FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/POLAR_BEAR/AInventoryComponent.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemsBase.h"
+#include "Components/ActorComponent.h"
+#include "AInventoryComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class POLAR_BEAR_API UAInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAInventoryComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Inventory")
+	TArray<AActor*> Inventory;
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void SetInventory(TArray<AActor*> NewInventory);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool bContains(AItemsBase* Item);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void RemoveItem(AItemsBase* Item);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void AddItem(AItemsBase* item);
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
