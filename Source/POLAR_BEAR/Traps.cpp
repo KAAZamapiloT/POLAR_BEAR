21aa8647da48100a5fc1f21efd217db47fd85ffa
Kazamapilot <kazamapilot@gmail.com>
2025-03-28 15:09:59 +0530

    WILL TRAPS MAKE GAME BETTER

---
+++ Source/POLAR_BEAR/Traps.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Traps.h"
+
+
+// Sets default values
+ATraps::ATraps()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATraps::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATraps::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATraps::Signal()
+{
+	IIA_intractable::Signal();
+}
+
+void ATraps::RevCast()
+{
+	IIA_intractable::RevCast();
+}
+
---
+++ Source/POLAR_BEAR/Traps.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <rapidjson/internal/meta.h>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include"IA_intractable.h"
+#include "Traps.generated.h"
+
+UENUM(Blueprintable)
+enum class ETrapType : uint8
+{
+SlowDown,
+	Blur,
+	Death
+};
+
+UCLASS()
+class POLAR_BEAR_API ATraps : public AActor,public IIA_intractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATraps();
+UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Traps")
+	ETrapType TrapType = ETrapType::SlowDown;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+    UFUNCTION(BlueprintCallable,Blueprintable,Category="Traps")
+	void Signal() override;
+	UFUNCTION(BlueprintCallable,BlueprintCallable,Category="Traps")
+	void RevCast() override;
+};
